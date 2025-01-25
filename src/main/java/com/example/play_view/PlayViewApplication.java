1346c8fa86e514024513d905dd716a6c3708c7b5
Olavo Miguel <olavomiguel423@gmail.com>
2025-01-25 11:01:09 -0300

    Project started

---
+++ src/main/java/com/example/play_view/PlayViewApplication.java
@@ -0,0 +1,13 @@
+package com.example.play_view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/play_view/PlayViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.play_view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
